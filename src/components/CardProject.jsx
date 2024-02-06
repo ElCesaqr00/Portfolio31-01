5a7691fa741cad9b1c9df4bc72edef9b1abbeec4
ElCesaqr00 <137964991+ElCesaqr00@users.noreply.github.com>
2024-02-06 11:23:13 -0500

    ajuste direccion imagenes

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,24 +1,22 @@
-import "./Styles/CardProject.css"
-import data from "../data/projects.json"
+import "./Styles/CardProject.css";
+import data from "../data/projects.json";
 
-const CardProject = ({jsonData}) => {
-
-  const cardList = data.map(dataList =>
+const CardProject = ({ jsonData }) => {
+  const cardList = data.map((dataList) => (
     <li key={dataList.id}>
-         <header className='card' >
-      <img src={dataList.image} alt="" />
-    </header> 
-    <div>
-      <h2> {dataList.name} </h2>
-      <p> {dataList.description} </p>
-    </div>
-    </li>)
+      <div className="card">
+        <header>
+          <img src={dataList.image} alt="" />
+        </header>
+        <div className="card_body">
+          <h2 className="card_body" > {dataList.name}</h2>
+          <p className="card_body" > {dataList.description} </p>
+        </div>
+      </div>
+    </li>
+  ));
 
-  return (
-    <ul className='card_container'>
-    { cardList }
-    </ul>
-  )
-}
+  return <ul className="card_container">{cardList}</ul>;
+};
 
-export default CardProject
+export default CardProject;
