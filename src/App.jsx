abbdd8a8452c0ddd8366dfd91d56123105ac92c4
ElCesaqr00 <137964991+ElCesaqr00@users.noreply.github.com>
2024-03-06 00:13:31 -0500

    ajustes de descripcion

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import CardCertificate from "./components/CardCertificate";
 import CardProject from "./components/CardProject";
 
 function App() {
-
   return (
     <>
-
-      <main >
+      <main>
         <header className="mainPage">
           <nav className="nav_header">
             <ul className="nav_container">
@@ -26,20 +24,66 @@ function App() {
             </ul>
           </nav>
         </header>
-        <section >
+        <section>
           <article className="HomePage" id="homepageId">
             <div className="HomePage_Text">
               <h1 className="HomePage_Text_Name">Hola, soy Cesar</h1>
               <div className="text_container">
-                <p >
-                  ¡Hola! Soy un apasionado <span className="text_roll">Desarrollador Web FullStack</span> que utiliza tecnologías como <span className="text_roll">ReactJs, NodeJs, Express,</span> y manejo de datos con <span className="text_roll">Postgres</span>. Me esfuerzo por ser ejemplar en mi trabajo, creando soluciones innovadoras y eficientes.
+                <p>
+                  ¡Hola! Soy un apasionado{" "}
+                  <span className="text_roll">Desarrollador Web FullStack</span>{" "}
+                  que utiliza tecnologías como{" "}
+                  <span className="text_roll">ReactJs, NodeJs, Express,</span> y
+                  manejo de datos con{" "}
+                  <span className="text_roll">Postgres</span>. Me esfuerzo por
+                  ser ejemplar en mi trabajo, creando soluciones innovadoras y
+                  eficientes.
                 </p>
               </div>
             </div>
             <div className="HomePage_Social">
-              <a className="Social_Links1" href="https://www.linkedin.com/in/cesar-david-montes-serna-b62724262/" target="_blank" ><div className="social_section"><img className="social_icon" src="../images/linkedin.png" alt="" /><p>Linkedin</p></div></a>
-              <a className="Social_Links1" href="https://github.com/ElCesaqr00" target="_blank"><div className="social_section"><img className="social_icon" src="../images/github.png" alt="" /><p>Github</p></div></a>
-              <a className="Social_Links1" href="www.google.com" target="_blank"><div className="social_section"><img className="social_icon" src="../images/gmail.png" alt="" /><p>Email</p></div></a>
+              <a
+                className="Social_Links1"
+                href="https://www.linkedin.com/in/cesar-david-montes-serna-b62724262/"
+                target="_blank"
+              >
+                <div className="social_section">
+                  <img
+                    className="social_icon"
+                    src="../images/linkedin.png"
+                    alt=""
+                  />
+                  <p>Linkedin</p>
+                </div>
+              </a>
+              <a
+                className="Social_Links1"
+                href="https://github.com/ElCesaqr00"
+                target="_blank"
+              >
+                <div className="social_section">
+                  <img
+                    className="social_icon"
+                    src="../images/github.png"
+                    alt=""
+                  />
+                  <p>Github</p>
+                </div>
+              </a>
+              <a
+                className="Social_Links1"
+                href="www.google.com"
+                target="_blank"
+              >
+                <div className="social_section">
+                  <img
+                    className="social_icon"
+                    src="../images/gmail.png"
+                    alt=""
+                  />
+                  <p>Email</p>
+                </div>
+              </a>
             </div>
           </article>
           <article className="projects_container" id="projectCards">
@@ -51,21 +95,27 @@ function App() {
           <article className="certificate_container" id="certificateCards">
             <h1 className="certificate_title">Certificates</h1>
             <div className="certificate_info">
-            <CardCertificate />
+              <CardCertificate />
             </div>
           </article>
           <article className="info_aboutMe">
             <div className="info_aboutMe_text" id="info_aboutMe">
               <h2>Cesar David Montes Serna</h2>
-              <p>¡Hola de nuevo! Me considero una persona dinámica, proactiva y muy responsable en las responsabilidades encomendadas a mí. Me gusta investigar información que tenga que ver con tecnología. He emprendido esta travesía como desarrollador, permitiéndome cumplir este deseo de pertenecer a esta industria. Espero ser de su agrado. </p>
+              <p>
+                ¡Hola de nuevo! Me considero una persona dinámica, proactiva y
+                muy responsable en las tareas encomendadas a mí. Me gusta
+                investigar información que tenga que ver con tecnología. He
+                emprendido esta travesía como desarrollador, permitiéndome
+                cumplir este deseo de pertenecer a esta industria. Espero ser de
+                su agrado y estoy emocionado por seguir creciendo en esta
+                apasionante área. ¡Gracias por visitar mi portafolio!
+              </p>
             </div>
           </article>
         </section>
       </main>
     </>
   );
-
 }
-
 
 export default App;
