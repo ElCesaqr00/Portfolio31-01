9f2a016d651b3b2bf316c528277c2c929704d179
Cesar David Montes <Cesarmontes0842@gmail.com>
2024-03-14 20:16:28 -0500

    ajustes

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -20,29 +20,29 @@ const CardProject = () => {
 
   const cardList = dataInfo.map(dataList =>
     <li className='card_info' key={dataList.id} itemID={dataList.name}>
-      <div>
+      <section>
         <header className='card' >
           <img src={dataList.image} className='card_img' alt="" />
         </header>
-        <div className='card_body'>
+        <article className='card_body'>
           <ul>
             <li className='card_title'> {dataList.name} </li>
             <li className='card_description'> {dataList.description} </li>
           </ul>
-          <div className='card_Links' >
-            <div>
+          <article className='card_Links' >
+            <article>
               <a href={dataList.Render}  target='_blank'  className='icon-links' >
               <i class="fa-solid fa-link"></i>
               </a>
-            </div>
-            <div>
+            </article>
+            <article>
               <a href={dataList.GitHub} target='_blank'  className='icon-links' >
               <i class="fa-brands fa-github"></i>
               </a>
-            </div>
-          </div>
-        </div>
-      </div>
+            </article>
+          </article>
+        </article>
+      </section>
       
     </li>)
     
